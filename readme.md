fcabd5407bd6bb5a3dfae6ab5952df0d8470b2ae
harrygr <harry@grumbar.me>
2017-07-20 12:13:34 +0100

    First Commit 🎉

---
+++ readme.md
@@ -0,0 +1,14 @@
+# Expressi
+
+A server-rendered web app built in Express and TypeScript
+
+## Dev setup
+
+- Install dependencies: `yarn install`
+- Run the server: `yarn dev`
+- Visit http://localhost:3000
+
+## Production Build
+
+- Compile to JS: `yarn build`
+- Serve it up: `yarn serve`
---
+++ src/routes/users.ts
@@ -0,0 +1,10 @@
+import * as express from 'express'
+import { Request, Response, NextFunction } from 'express'
+var router = express.Router()
+
+/* GET users listing. */
+router.get('/', function (req: Request, res: Response, next: NextFunction) {
+  res.send('respond with a resource');
+});
+
+export default router
